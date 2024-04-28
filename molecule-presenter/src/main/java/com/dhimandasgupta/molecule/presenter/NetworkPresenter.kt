19a1e86d7480409d57e4071b1968800dc95e7e76
dhimandasgupta <dhiman.dasgupta1984@gmail.com>
2024-04-28 23:14:26 +0530

    Fixed imports.

--- molecule-presenter/src/main/java/com/dhimandasgupta/molecule/presenter/NetworkPresenter.kt
+++ molecule-presenter/src/main/java/com/dhimandasgupta/molecule/presenter/NetworkPresenter.kt
@@ -6,13 +6,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import com.dhimandasgupta.common.android.ConnectionState
 import com.dhimandasgupta.state.machines.NetworkStateMachine
 
 class NetworkPresenter(
     private val networkStateMachines: NetworkStateMachine
 ) {
     @Composable
-    fun uiModel(): com.dhimandasgupta.common.android.ConnectionState {
+    fun uiModel(): ConnectionState {
         var connectionState by remember {
             mutableStateOf(networkStateMachines.immediateConnectedState())
         }
